87bef9bf902cb7906ff4f8a54401d86ff03dc63d
xiaobo <164419221@qq.com>
2020-07-26 00:00:03 +0800

    '分类完成一半'

--- src/pages/classify/index.jsx
+++ src/pages/classify/index.jsx
@@ -1,10 +1,53 @@
 import React, { Component } from 'react'
-
+import "./index.css"
+import {getCateGroy} from "../../config/api/index"
 export default class index extends Component {
+  state = {
+    active:"",
+    cateGroys:[],//请求的数据
+  }
+  async componentDidMount(){
+     //获取分类数据
+    let result = await getCateGroy()
+    this.setState({
+      cateGroys: result.data,
+      active:result.data[0].name
+    })
+  }
   render() {
+    const {cateGroys,active} = this.state
     return (
-      <div>
-        我是分类
+      <div className="classSearch"> 
+        {/* <!-- 搜索 --> */}
+        <div className="search" >
+          <div className="in">
+            <i></i>
+            <span>搜索商品, 共34101款好物</span>
+          </div>
+        </div>
+        {/* <!-- 内容 --> */}
+        <div className="content">
+          <div className="left">
+            <ul>
+              {
+                cateGroys&&cateGroys.map((item,index)=>{
+                  return (
+                    <li className={active===item.name?'active':''} key={index}>{item.name}</li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          <div className="right" v-if="cateGroy.subCateList">
+            <div className="titleImage">
+              <img />
+            </div>
+              <div className="imageItem">
+                < img />
+                <span></span>
+              </div>
+          </div>
+        </div>
       </div>
     )
   }
