b7190959a66935ea38d913e9aa715c74bbaca7db
xiaobo <164419221@qq.com>
2020-07-26 18:55:40 +0800

    '购物车完成'

--- src/pages/carShop/index.jsx
+++ src/pages/carShop/index.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
-import Swiper from "../../components/waterfall"
+import "./index.css"
 class index extends Component {
   render() {
     return (
-      <Swiper/>
+      <div className="carShop">
+        {/* <!-- 头部区域 --> */}
+        <div className="carShopHeader">
+          <p>购物车</p>
+          <p>领券</p>
+        </div>
+        {/* <!-- 说明三标识 --> */}
+        <div className="servicePolicy">
+          <div className="item">
+            <i className=""></i>
+            <span>30天无忧退货</span>
+          </div>
+          <div className="item ">
+            <i className=""></i>
+            <span>48小时快速退款</span>
+          </div>
+          <div className="item">
+            <i></i>
+            <span>满99元免邮费</span>
+          </div>
+        </div>
+        {/* <!-- 购物车图标 --> */}
+        <img src="http://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/noCart-d6193bd6e4.png?imageView&type=webp" />
+        {/* <!-- 登录按钮 --> */}
+        <div className="login">登录</div>
+      </div>
     );
   }
 }
