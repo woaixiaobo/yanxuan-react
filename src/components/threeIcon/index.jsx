a19b7b3d6e403e3c02e924b79677a12925c6042c
xiaobo <164419221@qq.com>
2020-07-26 12:32:50 +0800

    '值得买头部三个图标封装完成'

---
+++ src/components/threeIcon/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import "./index.css"
+import PropTypes from "prop-types";
+import {withRouter} from "react-router-dom"
+@withRouter
+class index extends Component {
+  static propTypes = {
+    title:PropTypes.string.isRequired,
+    background:PropTypes.string,
+  }
+  static defaultProps = {
+    background:'#fff'
+  }
+   //回到主页
+  toHome=()=>{
+    this.props.history.push('/home')
+    // this.$bus.$emit('isPersonal',true)
+  }
+  //去搜索
+  toSearch=()=>{
+    //当前页面路径
+    let now = this.props.match.path.split('/')[1]
+    // this.$bus.$emit('isPersonal',false)
+    this.props.history.push(`/search?perRoute=${now}`)
+  }
+  //去购物车
+  toCarShop=()=>{
+    this.props.history.push('/carShop')
+    // this.$bus.$emit('isPersonal',true)
+  }
+
+  render() {
+    const {title,background} = this.props
+    console.log(title,background);
+    return (
+      <div className="buyHeader" style={{backgroundColor:background}}>
+        <span className="homeIcon" onClick={this.toHome}></span>
+        <span className="text">{title}</span>
+        <div className="twoIcon">
+          <span onClick={this.toSearch}></span>
+          <span onClick={this.toCarShop}></span>
+        </div>
+      </div>
+    )
+  }
+}
+export default index
--- src/pages/deserveBuy/index.jsx
+++ src/pages/deserveBuy/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import ThreeIcon from "../../components/threeIcon/index"
 class index extends Component {
   render() {
     return (
-      <div>
-        我是值得买
+      <div className="buy">
+        {/* 头部 */}
+        <ThreeIcon title="值得买"/>
       </div>
     );
   }
